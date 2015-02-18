e91188d7ec94ae1e4b76923fb201b753f97ec7dc
yonathan9669 <yonathanmorales@gmail.com>
2015-02-18 05:05:05 -0430

    2 Step Verification Supported - Minimum corrections to URL and data reception

--- app.js
+++ app.js
@@ -10,48 +10,47 @@ var dns = new DNSimple(settings);
 // the update event is fired when there is a mismatch of
 // IP addresses
 lookup.on('update', function(public_ip) {
-  writeln("IP address mismatch:");
-  writeln("\tCurrent IP: " + settings.currentIP);
-  writeln("\t Public IP: " + public_ip);
-  writeln("Updating record...");
+    writeln("IP address mismatch:");
+    writeln("\tCurrent IP: " + settings.currentIP);
+    writeln("\t Public IP: " + public_ip);
+    writeln("Updating record...");
 
-  dns.update(public_ip);
+    dns.update(public_ip);
 })
 // the match event is called when the current and the public
 // IP addresses match
-.on('match', function(current_ip, public_id) {
-  writeln("IP addresses match.  No need to update.");
-});
+    .on('match', function (current_ip, public_id) {
+        writeln("IP addresses match.  No need to update.");
+    });
 
 // the updated event is raised after the public IP
 // was successfully updated
 dns.on('updated', function(data) {
-  writeln("Domain IP updated successfully.");
+    writeln("Domain IP updated successfully.");
 
-  var parentRecord = JSON.parse(data);
-  settings.currentIP = parentRecord.record.content;
+    var parentRecord = JSON.parse(data);
+    settings.currentIP = parentRecord.record.content;
 
-  writeln("Updating local settings...");
+    writeln("Updating local settings...");
 
-  // we have to save the new public IP to a local file
-  // so that we can do a comparison on the next run
-  fs.writeFile("./settings.json", JSON.stringify(settings), function(err, fd) {
-    if(err) throw err;
-  });
+    // we have to save the new public IP to a local file
+    // so that we can do a comparison on the next run
+    fs.writeFile("./settings.json", JSON.stringify(settings, undefined, 4), function (err, fd) {
+        if (err) throw err;
+    });
 
-  writeln("Local settings updated successfully.");
+    writeln("Local settings updated successfully.");
 
 }).on('error', function(e) {
-  writeln("Error: " + e);
+    writeln("Error: " + e);
 });
 
 function writeln(str) {
-  process.stdout.write(str + "\n");
+    process.stdout.write(str + "\n");
 }
 
 lookup.compare();
 
 // Uncomment the below to run continuously
 //  current set to run once an hour
-//setInterval(lookup.compare, 3600000);
-
+//setInterval(lookup.compare, 10000);
